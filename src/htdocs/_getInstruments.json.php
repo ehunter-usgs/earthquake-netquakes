eee6e45712e9a33f010e9093b497c93e65c79ee7
ehunter <ehunter@usgs.gov>
2025-03-14 12:22:10 -0600

    fix php errors, get application to run locally

--- src/htdocs/_getInstruments.json.php
+++ src/htdocs/_getInstruments.json.php
@@ -1,66 +1,72 @@
 <?php
 
-include_once '../conf/config.inc.php'; // app config
-include_once '../lib/_functions.inc.php'; // app functions
-include_once '../lib/classes/Db.class.php'; // db connector, queries
+include_once '../conf/config.inc.php';
+include_once '../lib/_functions.inc.php';
+include_once '../lib/classes/Db.class.php';
 
 $callback = safeParam('callback');
 $now = date(DATE_RFC2822);
 
 $db = new Db;
 
-// Db query results: instruments
+// Query instruments
 $rsInstruments = $db->queryInstruments();
 
-// Db query results: plots
+// Query plots
 $rsPlots = $db->queryPlots();
 
-// Initialize array template for json feed
+// Initialize JSON output array
 $output = [
-  'generated' => $now,
-  'count' => $rsInstruments->rowCount(),
-  'type' => 'FeatureCollection',
-  'features' => []
+    'generated' => $now,
+    'count' => $rsInstruments->rowCount(),
+    'type' => 'FeatureCollection',
+    'features' => []
 ];
 
-// Store plot metadata by site
-$plots = $rsPlots->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC); // group by site (1st column)
+// Group plot metadata by site
+$plots = $rsPlots->fetchAll(PDO::FETCH_GROUP | PDO::FETCH_ASSOC);
 
-// Store results from Instruments / Plots tables in features array
+// Process instrument data and add to features array
 while ($row = $rsInstruments->fetch(PDO::FETCH_ASSOC)) {
-  $datetime = '';
-  $file = '';
-  $site = $row['site'];
-  $plot = $plots[$site][0];
+    $datetime = '';
+    $file = '';
+    $site = $row['site'];
 
-  if ($plot) {
-    $datetime = $plot['datetime'];
-    $file = $plot['file'];
-  }
+    // Check if plots exist for the current site
+    if (isset($plots[$site]) && is_array($plots[$site]) && !empty($plots[$site])) {
+        $plot = $plots[$site][0]; // Get the first plot for the site
+        if(is_array($plot)){
+            $datetime = $plot['datetime'];
+            $file = $plot['file'];
+        }
 
-  $feature = [
-    'geometry' => [
-      'coordinates' => [
-        floatval($row['lon']),
-        floatval($row['lat'])
-      ],
-      'type' => 'Point'
-    ],
-    'id' => 'point' . intval($row['id']),
-    'properties' => [
-      'site' => $site,
-      'net' => $row['net'],
-      'loc' => $row['loc'],
-      'description' => $row['description'],
-      'status' => $row['status'],
-      'file' => $file,
-      'datetime' => $datetime
-    ],
-    'type' => 'Feature'
-  ];
+    }
 
-  array_push($output['features'], $feature);
+    $feature = [
+        'geometry' => [
+            'coordinates' => [
+                floatval($row['lon']),
+                floatval($row['lat'])
+            ],
+            'type' => 'Point'
+        ],
+        'id' => 'point' . intval($row['id']),
+        'properties' => [
+            'site' => $site,
+            'net' => $row['net'],
+            'loc' => $row['loc'],
+            'description' => $row['description'],
+            'status' => $row['status'],
+            'file' => $file,
+            'datetime' => $datetime
+        ],
+        'type' => 'Feature'
+    ];
+
+    array_push($output['features'], $feature);
 }
 
-// Send json stream to browser
+// Send JSON response
 showJson($output, $callback);
+
+?>
